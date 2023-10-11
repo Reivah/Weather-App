76e0b50747d596c831677930d6cd123de4d869f7
Jakub <havier1993@gmail.com>
2023-10-11 11:38:02 +0200

    downloading all variables

---
+++ main.js
@@ -0,0 +1,16 @@
+const citySearch = document.querySelector('.city-search')
+const submitBtn = document.querySelector('.search-btn')
+const cityResult = document.querySelector('.city-result')
+const date = document.querySelector('.date')
+const imageBox = document.querySelector('.image-box')
+const mainTemp = document.querySelector('.temp')
+const couldsInfo = document.querySelector('.clouds')
+const feelsTemp = document.querySelector('.feels-temp')
+const humidity = document.querySelector('.humidity')
+const windSpeed = document.querySelector('.wind-speed')
+const windDeg = document.querySelector('.wind-deg')
+const pressure = document.querySelector('.pressure')
+
+
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={a74b4223c04fb2978fa22db3652f8b6d}&units=metric&land={pl}
